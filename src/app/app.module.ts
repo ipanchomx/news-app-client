14816eb17f1c163507b6fb9a5ad29470f5c139d8
Edgar Medina <is715468@iteso.mx>
2020-11-02 22:19:00 -0600

    Setup app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NoticiasComponent } from './pages/noticias/noticias.component';
+import { TitularesComponent } from './pages/titulares/titulares.component';
+import { Page404Component } from './pages/page404/page404.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NoticiasComponent,
+    TitularesComponent,
+    Page404Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
