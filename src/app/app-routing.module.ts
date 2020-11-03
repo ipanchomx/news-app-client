9d7fb10a688d07c36c29e80f766cdb2ce6f1de41
Edgar Medina <is715468@iteso.mx>
2020-11-02 22:16:32 -0600

    Add new routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { NoticiasComponent } from './pages/noticias/noticias.component';
+import { TitularesComponent } from './pages/titulares/titulares.component';
+import { Page404Component } from './pages/page404/page404.component';
+
+const routes: Routes = [
+  { path : "", redirectTo : 'noticias', pathMatch : 'full'},
+  { path : "noticias", component : NoticiasComponent},
+  { path : "titulares", component : TitularesComponent},
+  { path : "**", component : Page404Component}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
